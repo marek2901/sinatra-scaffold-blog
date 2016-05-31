9e4e0176a5809928ace391827d25c66d0e8ef6af
Marek Dz <marek2901@gmail.com>
2016-06-01 01:09:26 +0200

    simple sinatra blog part 2

--- Gemfile
+++ Gemfile
@@ -11,4 +11,5 @@ group :development do
   gem "rake", "~> 10.0"
   gem "minitest", "~> 5.2"
   gem "rack-test", "~> 0.6"
+  gem "racksh", "~> 1.0.0"
 end
--- hello_blog.rb
+++ hello_blog.rb
@@ -1,5 +1,9 @@
 class HelloBlog < Sinatra::Base
 
+  ## IMPORTANT set this to true so you can set request method in form using hidden field
+  ## modern browser don't support put delete patch requests :(
+  set :method_override, true
+  
   set :public_folder => "public", :static => true
 
   get "/" do
@@ -10,5 +14,29 @@ class HelloBlog < Sinatra::Base
     @post = Post.find(id: params[:id])
     erb :post
   end
-  
+
+  get "/post/new" do
+    erb :form_post
+  end
+
+  post "/posts" do
+    Post.new(params[:post]).save
+    redirect "/"
+  end
+
+  get '/post/:id/edit' do
+    @post = Post.find(id: params[:id])
+    erb :form_post
+  end
+
+  put '/posts/:id' do
+    Post.find(id: params[:id]).set(params[:post]).save
+    redirect "/"
+  end
+
+  delete '/posts/:id' do
+    Post.find(id: params[:id]).delete
+    redirect "/"
+  end
+
 end
